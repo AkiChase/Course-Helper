f2d391c9dda35deff92d49c4f960c199a8416f70
如初 <1003019131@qq.com>
2022-05-08 13:19:28 +0800

    navigationBar and navButton interface

---
+++ app/src/components/NavButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <router-link custom v-slot="{ navigate, isExactActive }" :to="to">
+    <div @click="navigate" :class="isExactActive ? 'router-link router-link-exact-active' : 'router-link'">
+      <n-icon :size="35" color="#eee">
+        <slot></slot>
+      </n-icon>
+    </div>
+  </router-link>
+</template>
+
+<script>
+import {NIcon} from "naive-ui";
+
+export default {
+  name: "NavButton",
+  props: ['to'],
+  components: {
+    NIcon
+  }
+}
+</script>
+
+<style scoped>
+
+.router-link {
+  width: 100%;
+  text-align: center;
+  padding: 8px 0;
+  cursor: pointer;
+}
+
+.router-link:not(.router-link-exact-active):hover {
+  background-color: #36ad6a;
+  transition: background-color 0.5s;
+}
+
+
+.router-link-exact-active {
+  background-color: #0c7a43;
+  transition: background-color 0.5s;
+}
+
+</style>
--- app/src/components/NavigationBar.vue
+++ app/src/components/NavigationBar.vue
@@ -1,21 +1,56 @@
 <template>
-  <div>
-    <h2>导航栏</h2>
+  <div class="main" :style="`background-color: ${themeVars.primaryColor}`">
+    <div class="logo">
+      <img src="../assets/logo.png" alt=""/>
+    </div>
+
+    <nav-button :to="{name:'login'}">
+      <settings-outline/>
+    </nav-button>
+    <nav-button :to="{name:'home'}">
+      <settings-outline/>
+    </nav-button>
+
   </div>
-  <!--    <router-link to="/">Home</router-link>-->
-  <!--    <router-link to="/about">About</router-link>-->
-  <!--  <router-view></router-view>-->
 </template>
 
 <script>
 
 
+import {useThemeVars} from "naive-ui";
+import Login from "@/views/Login";
+import NavButton from "@/components/NavButton";
+import {SettingsOutline} from "@vicons/ionicons5";
+
 export default {
   name: "NavigationBar",
+  components: {
+    Login,
+    SettingsOutline,
+    NavButton
+  },
+  setup() {
+    const themeVars = useThemeVars()
 
+    return {
+      themeVars
+    }
+  }
 }
 </script>
 
 <style scoped>
+.main{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
+.logo {
+  padding: 0 10px;
+}
+
+.logo > img {
+  width: 100%;
+}
 </style>
