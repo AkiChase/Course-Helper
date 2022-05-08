a0879d2f5889a9bf82060792113eeb62b2f5413d
如初 <1003019131@qq.com>
2022-05-08 13:17:34 +0800

    add builder options

--- app/vue.config.js
+++ app/vue.config.js
@@ -3,7 +3,21 @@ module.exports = defineConfig({
     transpileDependencies: true,
     pluginOptions: {
         electronBuilder: {
-            preload: "src/preload.js"
+            preload: "src/preload.js",
+            builderOptions: {
+                "appId": "com.ruchuby.course",
+                "productName": "CourseHelper",
+                "win": {
+                    "icon": "dist_electron/icons/logo.ico"
+                },
+                "nsis": {
+                    "oneClick": false,
+                    "perMachine": true,
+                    "allowElevation": true,
+                    "allowToChangeInstallationDirectory": true,
+                    "createDesktopShortcut": true
+                }
+            }
         }
     }
 })
