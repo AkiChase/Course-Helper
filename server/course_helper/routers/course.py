a121d65f653d83fc6ad79b15a7be14852e326243
如初 <1003019131@qq.com>
2022-05-21 11:56:22 +0800

    api course resource
    getCourseResourceInfo

--- server/course_helper/routers/course.py
+++ server/course_helper/routers/course.py
@@ -1,16 +1,29 @@
+import os
 import re
 
 import nanoid
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from lxml import etree
 from pydantic import BaseModel
 
 from .user import User
 from ..common import success_info, CourseHelperException, error_info
 from ..logger import Logger
+from ..download import Downloader
 
 router = APIRouter()
 logger: Logger
+
+
+class FileModel(BaseModel):
+    file_id: str
+    res_id: str
+    file_dir: str = './'
+
+
+class DownloadFilesModel(BaseModel):
+    file_list: list[FileModel]
+    dir_path: str
 
 
 @router.on_event("startup")
@@ -179,6 +192,73 @@ async def get_course_resource(course_id: str, folder_id: str = '0', deep: bool =
         raise HTTPException(400, detail=error_info('获取课程资源失败'))
 
 
+@router.get('/getCourseResourceInfo')
+async def get_course_resource_info(file_id: str, res_id: str):
+    """
+    获取课程资源信息
+    """
+    try:
+        session = await User.get_login_session()
+        res = session.get('https://course2.xmu.edu.cn/meol/common/script/preview/'
+                          f'download_preview.jsp?fileid={file_id}&resid={res_id}')
+        html = etree.HTML(res.text)
+        elements = html.xpath('//div[@class="h1-title"]//span')
+        info = {
+            'file_name': elements[1].text.strip(),
+            'file_size': elements[2].text.strip().strip('()\n')
+        }
+        return success_info('获取课程资源信息成功', data=info)
+
+    except CourseHelperException as e:
+        logger.warning(f'获取课程资源信息失败 - 失败原因:{e}')
+        raise HTTPException(400, detail=error_info(e.data))
+    except Exception as e:
+        logger.debug(f'获取课程资源信息失败 e-{e}')
+        raise HTTPException(400, detail=error_info('获取课程资源信息失败'))
+
+
+@router.post('/downloadCourseFiles')
+async def download_course_files(data: DownloadFilesModel, background_tasks: BackgroundTasks):
+    """
+    下载文件
+    """
+    try:
+        Downloader.check_dir(data.dir_path)  # 检查文件夹是否存在
+        session = await User.get_login_session()
+
+        file_list = data.file_list
+        download_info = []
+
+        for item in file_list:
+            file_info = await Downloader.get_file_info(session, item.file_id, item.res_id)
+            if file_info['success']:
+                while True:
+                    file_path = os.path.join(data.dir_path, file_info['file_name'])
+                    if os.path.exists(file_path):
+                        # 文件名重复则添加一个#
+                        file_name_no_ext = file_info['file_name'][0:file_info['file_name'].rfind('.')]
+                        file_info['file_name'] = f"{file_name_no_ext}#.{file_info['file_ext']}"
+                    else:
+                        break
+
+                download_id = nanoid.generate()
+                file_info['download_id'] = download_id
+                file_info['file_path'] = file_path
+                # 提交BackgroundTasks 避免阻塞当前进程
+                background_tasks.add_task(Downloader.download_file, download_id, item.file_id, item.res_id, file_path)
+                download_info.append(file_info)
+                logger.success(f'下载任务创建成功 download_id:{download_id} size:{file_info["file_size"]} path:{file_path}')
+
+        return success_info('文件已添加到下载列表', data=download_info)
+
+    except CourseHelperException as e:
+        logger.warning(f'文件下载失败 - 失败原因:{e}')
+        raise HTTPException(400, detail=error_info(e.data))
+    except Exception as e:
+        logger.debug(f'文件下载失败 e-{e}')
+        raise HTTPException(400, detail=error_info('文件下载失败'))
+
+
 def get_resource_in_folder(course_id, folder_id, s, deep_flag=False) -> list:
     res = s.get(f'https://course2.xmu.edu.cn/meol/common/script/listview.jsp?folderid={folder_id}&lid={course_id}')
     html = etree.HTML(res.text)
@@ -200,7 +280,7 @@ def get_resource_in_folder(course_id, folder_id, s, deep_flag=False) -> list:
             if deep_flag:
                 res_obj['children'] = get_resource_in_folder(course_id, res_obj['folder_id'], s, deep_flag)
         else:
-            res_obj['fileid'], res_obj['res_id'] = re.search(r'fileid=(\d*).*?resid=(\d*)', res_url).group(1, 2)
+            res_obj['file_id'], res_obj['res_id'] = re.search(r'fileid=(\d*).*?resid=(\d*)', res_url).group(1, 2)
         content.append(res_obj)
     return content
 
